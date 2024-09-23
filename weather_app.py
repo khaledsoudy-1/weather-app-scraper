2836de763b155f91158368286c38e1ff3af1a337
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-23 18:27:06 +0300

    Retrieve and display weather data using requests module.

--- weather_app.py
+++ weather_app.py
@@ -1,7 +1,7 @@
 # A program that REQUESTS current weather from a web service.
 import os
 from dotenv import load_dotenv
-
+import requests
 
 # Load environment variables
 load_dotenv()
@@ -15,12 +15,11 @@ def get_current_weather():
     # Create the request URL using the user's input and API key from environment variables.
     request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv("API_KEY")}&units=metric"
     
-    # This prints only the request URL, but the weather data is not yet fetched or retrieved.
-    print(request_url)
+    # Retrieve and display the weather data
+    weather_data = requests.get(request_url).json()
+    
+    print(weather_data)
     
 get_current_weather()
 
-
-# NOTE: IF you open this request_url link, it will display the weather data on a web page.
-# NOTE: IF you want to fetch (retrieve) weather date ==> use `requests` module.
 
