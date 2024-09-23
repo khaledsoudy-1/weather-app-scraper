4fdb1e1188d122a7e697ea310af3a01463788dc5
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-23 21:53:04 +0300

    Add error handling for invalid city names.

--- weather_app.py
+++ weather_app.py
@@ -14,24 +14,22 @@ def get_current_weather():
     city_name = input("Please, enter a city name: ")
 
     # Create the request URL using the user's input and API key from environment variables.
-    request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv("API_KEY")}&units=metric"
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={os.getenv('API_KEY')}&units=metric"
     
     # Retrieve and display the weather data
     weather_data = requests.get(request_url).json()
     
-    pprint(weather_data)
-    
-    # Display more readable data for users.
-    print(f"\nCurrent Weather for '{city_name.title()}' City - {weather_data['sys']['country']}:")
-    print(f"Temperature : {weather_data['main']['temp']}°C")
-    print(f"Feels like  : {weather_data['main']['feels_like']}°C")
-    print(f"Weather     : {weather_data['weather'][0]['description'].capitalize()}")
+    # Check if the request was successful
+    if weather_data['cod'] != 200:
+        print(f"\n❌  Error: {weather_data['message']}")
+        
+    else:
+        # Display more readable data for users.
+        print(f"\nCurrent Weather for '{city_name.title()}' City - {weather_data['sys']['country']}:")
+        print(f"Temperature : {weather_data['main']['temp']}°C")
+        print(f"Feels like  : {weather_data['main']['feels_like']}°C")
+        print(f"Weather     : {weather_data['weather'][0]['description'].capitalize()}")
 
     
 if __name__ == '__main__':
     get_current_weather()
-
-
-# NOTE: The Code for successful request ==> 'cod': 200
-# NOTE: Which means that any other code will refer to an error along with an error message
-# NOTE: For Example: If you pass any wrong city name.
